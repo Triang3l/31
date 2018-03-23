45d49853cfffd04ff1b426f683a8ca489a9eb28b
Triang3l <hwguy.siplus@gmail.com>
2018-03-23 15:16:16 +0300

    abAtomic: relaxed and acquire/release

--- Code/Tri1/abParallel/abParallel.h
+++ Code/Tri1/abParallel/abParallel.h
@@ -3,9 +3,28 @@
 
 #include "../abCommon.h"
 
+// Note: acquire is used like "check atomic -> acquire -> read dependencies",
+// release is used like "write dependencies -> release -> update atomic".
+
 #if defined(abPlatform_OS_Windows)
+#include <intrin.h>
 #include <synchapi.h>
 #include <winnt.h>
+
+#if defined(abPlatform_CPU_x86)
+#define abAtomic_Acquire _ReadBarrier
+#define abAtomic_Release _WriteBarrier
+#elif defined(abPlatform_CPU_Arm)
+#define abAtomic_Acquire() { __dmb(_ARM_BARRIER_ISH); _ReadBarrier(); }
+#define abAtomic_Release() { _WriteBarrier(); __dmb(_ARM_BARRIER_ISH); }
+#else
+#error No memory barriers defined for the target CPU on Windows.
+#endif
+
+abForceInline uint32_t abAtomic_LoadRelaxedU32(const uint32_t *location) { return *((volatile const uint32_t *) location); }
+abForceInline uint64_t abAtomic_LoadRelaxedU64(const uint64_t *location) { return *((volatile const uint64_t *) location); }
+abForceInline void abAtomic_StoreRelaxedU32(uint32_t *location, uint32_t value) { *((volatile uint32_t *) location) = value; }
+abForceInline void abAtomic_StoreRelaxedU64(uint64_t *location, uint64_t value) { *((volatile uint64_t *) location) = value; }
 
 #define abParallel_YieldOnCore YieldProcessor
 #define abParallel_YieldOnAllCores() Sleep(0)
@@ -34,6 +53,18 @@ typedef CONDITION_VARIABLE abParallel_CondEvent;
 
 #else
 #error No parallelization API implementation for the target OS.
+#endif
+
+abForceInline int32_t abAtomic_LoadRelaxedS32(const int32_t *location) { return (int32_t) abAtomic_LoadRelaxedU32((const uint32_t *) location); }
+abForceInline int64_t abAtomic_LoadRelaxedS64(const int64_t *location) { return (int32_t) abAtomic_LoadRelaxedU64((const uint64_t *) location); }
+abForceInline void abAtomic_StoreRelaxedS32(int32_t *location, int32_t value) { abAtomic_StoreRelaxedU32((uint32_t *) location, (uint32_t) value); }
+abForceInline void abAtomic_StoreRelaxedS64(int64_t *location, int64_t value) { abAtomic_StoreRelaxedU64((uint64_t *) location, (uint64_t) value); }
+#ifdef abPlatform_CPU_64Bit
+abForceInline void *abAtomic_LoadRelaxedPtr(void * const *location) { return (void *) abAtomic_LoadRelaxedU64((const uint64_t *) location); }
+abForceInline void abAtomic_StoreRelaxedPtr(void **location, void *value) { abAtomic_StoreRelaxedU64((uint64_t *) location, (uint64_t) value); }
+#else
+abForceInline void *abAtomic_LoadRelaxedPtr(void * const *location) { return (void *) abAtomic_LoadRelaxedU32((const uint32_t *) location); }
+abForceInline void abAtomic_StoreRelaxedPtr(void **location, void *value) { abAtomic_StoreRelaxedU32((uint32_t *) location, (uint32_t) value); }
 #endif
 
 #endif
