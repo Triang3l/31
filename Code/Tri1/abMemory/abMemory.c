b3cd47530335b210a13a90f3039aed39a20efe70
Triang3l <hwguy.siplus@gmail.com>
2018-03-24 23:03:13 +0300

    abMemory: malloc wrapper providing alignment and grouped freeing

---
+++ Code/Tri1/abMemory/abMemory.c
@@ -0,0 +1,177 @@
+#include "abMemory.h"
+#include "../abFeedback/abFeedback.h"
+#include "../abText/abText.h"
+#include <stdlib.h>
+
+abParallel_Mutex abMemory_TagList_Mutex;
+abMemory_Tag * abMemory_TagList_First, * abMemory_TagList_Last;
+
+#define abMemoryi_Allocation_AlignmentType_Got8Aligned 0xab08u
+#define abMemoryi_Allocation_AlignmentType_Got16Aligned 0xab16u
+
+void abMemory_Init() {
+	if (!abParallel_Mutex_Init(&abMemory_TagList_Mutex)) {
+		abFeedback_Crash("abMemory_Init", "Failed to initialize the tag list mutex.");
+	}
+	abMemory_TagList_First = abMemory_TagList_Last = abNull;
+}
+
+abMemory_Tag * abMemory_Tag_Create(char const * name) {
+	abMemory_Tag * tag = (abMemory_Tag *) malloc(sizeof(abMemory_Tag));
+	if (tag == abNull) {
+		abFeedback_Crash("abMemory_Tag_Init", "Failed to allocate memory for tag %s.", name);
+	}
+	if (!abParallel_Mutex_Init(&tag->mutex)) {
+		abFeedback_Crash("abMemory_Tag_Init", "Failed to initialize the allocation list mutex for tag %s.", name);
+	}
+	abTextA_Copy(name, tag->name, abArrayLength(tag->name));
+	tag->allocationFirst = tag->allocationLast = abNull;
+	tag->totalAllocated = 0u;
+
+	abParallel_Mutex_Lock(&abMemory_TagList_Mutex);
+	tag->tagPrevious = abMemory_TagList_Last;
+	tag->tagNext = abNull;
+	if (abMemory_TagList_Last != abNull) {
+		abMemory_TagList_Last->tagNext = tag;
+	} else {
+		abFeedback_Assert(abMemory_TagList_First == abNull, "abMemory_Tag_Create", "If there's no last tag, there mustn't be the first one.");
+		abMemory_TagList_First = tag;
+	}
+	abMemory_TagList_Last = tag;
+	abParallel_Mutex_Unlock(&abMemory_TagList_Mutex);
+
+	return tag;
+}
+
+void abMemory_Tag_Destroy(abMemory_Tag * tag) {
+	abParallel_Mutex_Lock(&abMemory_TagList_Mutex);
+	if (tag->tagPrevious != abNull) {
+		tag->tagPrevious->tagNext = tag->tagNext;
+	} else {
+		abFeedback_Assert(abMemory_TagList_First == tag, "abMemory_Tag_Destroy", "If a tag has no previous, it must be the first.");
+		abMemory_TagList_First = tag->tagNext;
+	}
+	if (tag->tagNext != abNull) {
+		tag->tagNext->tagPrevious = tag->tagPrevious;
+	} else {
+		abFeedback_Assert(abMemory_TagList_Last == tag, "abMemory_Tag_Destroy", "If a tag has no next, it must be the last.");
+		abMemory_TagList_Last = tag->tagPrevious;
+	}
+	abParallel_Mutex_Unlock(&abMemory_TagList_Mutex);
+
+	abParallel_Mutex_Lock(&tag->mutex);
+	abMemory_Allocation * allocation, * nextAllocation;
+	for (allocation = tag->allocationLast; allocation != abNull; allocation = nextAllocation) {
+		nextAllocation = allocation->inTagPrevious;
+		#ifndef abPlatform_CPU_64Bit
+		if (allocation->alignmentType == abMemoryi_Allocation_AlignmentType_Got8Aligned) {
+			allocation = (abMemoryi_Allocation *) ((uint8_t *) allocation - 8u);
+		}
+		#endif
+		free(allocation);
+	}
+	abParallel_Mutex_Unlock(&tag->mutex);
+
+	abParallel_Mutex_Destroy(&tag->mutex);
+	free(tag); // Because it's created with Create, not Init, and Create does malloc.
+}
+
+void * abMemory_DoAlloc(abMemory_Tag * tag, size_t size, char const * fileName, unsigned int fileLine) {
+	// Malloc gives 16-aligned blocks on 64-bit platforms, 8-aligned on 32-bit.
+	#ifdef abPlatform_CPU_64Bit
+	void * block = malloc(sizeof(abMemory_Allocation) + size);
+	#else
+	void * block = malloc(sizeof(abMemory_Allocation) + 8u + size);
+	#endif
+	if (block == abNull) {
+		abFeedback_Crash("abMemory_DoAlloc", "Failed to allocate %zu for tag %s at %s:%u.", size, tag->name, fileName, fileLine);
+	}
+
+	abMemory_Allocation * allocation;
+	#ifdef abPlatform_CPU_64Bit
+	if (((size_t) block & 15u) != 0u) {
+		abFeedback_Crash("abMemory_DoAlloc", "Got a pointer from malloc that is not 16-aligned, this totally shouldn't happen!");
+	}
+	allocation = (abMemory_Allocation *) block;
+	allocation->alignmentType = abMemoryi_Allocation_AlignmentType_Got16Aligned;
+	#else
+	if (((size_t) block & 7u) != 0u) {
+		abFeedback_Crash("abMemory_DoAlloc", "Got a pointer from malloc that is not 8-aligned, this totally shouldn't happen!");
+	}
+	if (((size_t) block & 8u) != 0u) {
+		allocation = (abMemoryi_Allocation *) ((uint8_t *) block + 8u);
+		allocation->alignmentType = abMemoryi_Allocation_AlignmentType_Got8Aligned;
+	} else {
+		allocation = (abMemoryi_Allocation *) block;
+		allocation->alignmentType = abMemoryi_Allocation_AlignmentType_Got16Aligned;
+	}
+	#endif
+	allocation->size = size;
+	allocation->fileName = fileName;
+	allocation->fileLine = (uint16_t) fileLine;
+
+	allocation->tag = tag;
+	abParallel_Mutex_Lock(&tag->mutex);
+	allocation->inTagPrevious = tag->allocationLast;
+	allocation->inTagNext = abNull;
+	if (tag->allocationLast != abNull) {
+		tag->allocationLast->inTagNext = allocation;
+	} else {
+		abFeedback_Assert(tag->allocationFirst == abNull, "abMemory_DoAlloc", "If a tag has no last allocation, it mustn't have the first one.");
+		tag->allocationFirst = allocation;
+	}
+	tag->allocationLast = allocation;
+	tag->totalAllocated += size;
+	abParallel_Mutex_Unlock(&tag->mutex);
+
+	return allocation + 1u;
+}
+
+void abMemory_Free(void * memory) {
+	if (memory == abNull) {
+		return;
+	}
+
+	abMemory_Allocation * allocation = ((abMemory_Allocation *) memory - 1u);
+	void * block;
+	switch (allocation->alignmentType) {
+	case abMemoryi_Allocation_AlignmentType_Got16Aligned:
+		block = allocation;
+		break;
+	#ifndef abPlatform_CPU_64Bit
+	case abMemoryi_Allocation_AlignmentType_Got8Aligned:
+		block = (uint8_t *) allocation - 8u;
+		break;
+	#endif
+	default:
+		abFeedback_Crash("abMemory_Free", "Tried to free memory that wasn't allocated with abMemory_Alloc.");
+		return;
+	}
+
+	abMemory_Tag * tag = allocation->tag;
+	abParallel_Mutex_Lock(&tag->mutex);
+	tag->totalAllocated -= allocation->size;
+	if (allocation->inTagPrevious != abNull) {
+		allocation->inTagPrevious->inTagNext = allocation->inTagNext;
+	} else {
+		abFeedback_Assert(tag->allocationFirst == allocation, "abMemory_Free", "If an allocation has no previous, it must be the first in tag.");
+		tag->allocationFirst = allocation->inTagNext;
+	}
+	if (allocation->inTagNext != abNull) {
+		allocation->inTagNext->inTagPrevious = allocation->inTagPrevious;
+	} else {
+		abFeedback_Assert(tag->allocationLast == allocation, "abMemory_Free", "If an allocation has no next, it must be the last in tag.");
+		tag->allocationLast = allocation->inTagPrevious;
+	}
+	abParallel_Mutex_Unlock(&tag->mutex);
+
+	free(block);
+}
+
+void abMemory_Shutdown() {
+	// No need to lock the mutex - at this point, any interactions with abMemory are invalid.
+	while (abMemory_TagList_Last != abNull) {
+		abMemory_Tag_Destroy(abMemory_TagList_Last);
+	}
+	abParallel_Mutex_Destroy(&abMemory_TagList_Mutex);
+}
---
+++ Code/Tri1/abMemory/abMemory.h
@@ -0,0 +1,51 @@
+#ifndef abInclude_abMemory
+#define abInclude_abMemory
+#include "../abParallel/abParallel.h"
+
+/*
+ * An abstraction for memory allocation, inspired by the Qfusion engine <3
+ * Handles allocation failures (by crashing) and 16 byte alignment.
+ * Also allows for destruction of all allocations at once by destroying a pool.
+ */
+
+void abMemory_Init();
+
+#ifdef abPlatform_CPU_64Bit
+typedef struct abAligned(16u) abMemory_Allocation {
+#else
+typedef struct abAligned(8u) abMemory_Allocation {
+#endif
+	struct abMemory_Tag * tag; // 4 (32-bit) / 8 (64-bit) bytes.
+	struct abMemory_Allocation * inTagPrevious, * inTagNext; // 12/24 bytes.
+	size_t size; // 16/32 bytes.
+	char const * fileName; // Immutable. 20/40 bytes.
+	uint16_t fileLine; // 22/42 bytes.
+	uint16_t alignmentType; // 24/44 bytes. If Got8Aligned, there's a 8-byte padding before this structure in malloc block.
+} abMemory_Allocation;
+
+typedef struct abMemory_Tag {
+	char name[64u];
+
+	abParallel_Mutex mutex;
+	// Protected by the tag's mutex.
+	abMemory_Allocation * allocationFirst, * allocationLast;
+	size_t totalAllocated;
+
+	// Protected by abMemory_TagList_Mutex.
+	struct abMemory_Tag * tagPrevious, * tagNext;
+} abMemory_Tag;
+
+// Using Create rather than the usual Init to make sure tags created dynamically won't depend on other tags.
+abMemory_Tag * abMemory_Tag_Create(char const * name);
+void abMemory_Tag_Destroy(abMemory_Tag * tag);
+
+extern abParallel_Mutex abMemory_TagList_Mutex;
+extern abMemory_Tag * abMemory_TagList_First, * abMemory_TagList_Last;
+
+void * abMemory_DoAlloc(abMemory_Tag * tag, size_t size, char const * fileName, unsigned int fileLine);
+#define abMemory_Alloc(tag, size) abMemory_DoAlloc((tag), (size), __FILE__, __LINE__)
+void abMemory_Free(void * memory);
+
+void abMemory_Shutdown();
+
+#endif
