f06ab1a9efe73a5e610602cb2160e4dce49795ed
Triang3l <hwguy.siplus@gmail.com>
2018-03-27 13:04:57 +0300

    abGPU_RTStore: D3D

--- Code/Tri1/abGPU/abGPUi_D3D/abGPUi_D3D_RTStore.c
+++ Code/Tri1/abGPU/abGPUi_D3D/abGPUi_D3D_RTStore.c
@@ -94,4 +94,64 @@ bool abGPU_RTStore_SetColor(abGPU_RTStore * store, unsigned int rtIndex, abGPU_I
 	return true;
 }
 
+bool abGPU_RTStore_SetDepth(abGPU_RTStore * store, unsigned int rtIndex, abGPU_Image * image,
+		unsigned int layer, unsigned int side, unsigned int mip, bool readOnly) {
+	if (rtIndex >= store->countDepth || !(image->typeAndDimensions & abGPU_Image_Type_Renderable) ||
+			!abGPU_Image_Format_IsDepth(image->format) || mip >= image->mips) {
+		return false;
+	}
+	D3D12_DEPTH_STENCIL_VIEW_DESC desc;
+	desc.Format = abGPUi_D3D_Image_FormatToDepthStencil(image->format);
+	desc.Flags = (readOnly ? (D3D12_DSV_FLAG_READ_ONLY_DEPTH | D3D12_DSV_FLAG_READ_ONLY_STENCIL) : D3D12_DSV_FLAG_NONE);
+	abGPU_Image_Dimensions dimensions = abGPU_Image_GetDimensions(image);
+	switch (dimensions) {
+	case abGPU_Image_Dimensions_2D:
+		layer = side = 0u;
+		desc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+		desc.Texture2D.MipSlice = mip;
+		break;
+	case abGPU_Image_Dimensions_2DArray:
+	case abGPU_Image_Dimensions_Cube:
+	case abGPU_Image_Dimensions_CubeArray:
+		{
+			unsigned int arraySlice = 0u;
+			if (abGPU_Image_Dimensions_AreArray(dimensions)) {
+				if (layer >= image->d) { return false; }
+				arraySlice = layer;
+			} else {
+				layer = 0u;
+			}
+			if (abGPU_Image_Dimensions_AreCube(dimensions)) {
+				if (side >= 6) { return false; }
+				arraySlice = arraySlice * 6u + side;
+			} else {
+				side = 0u;
+			}
+			desc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2DARRAY;
+			desc.Texture2DArray.MipSlice = mip;
+			desc.Texture2DArray.FirstArraySlice = arraySlice;
+			desc.Texture2DArray.ArraySize = 1u;
+		}
+		break;
+	default:
+		return false;
+	}
+	abGPU_RTStore_RT * rt = &store->renderTargets[store->countColor + rtIndex];
+	rt->image = image;
+	rt->layer = layer;
+	rt->side = side;
+	rt->mip = mip;
+	ID3D12Device_CreateDepthStencilView(abGPUi_D3D_Device, image->i_resource, &desc, abGPUi_D3D_RTStore_GetDescriptorHandleDepth(store, rtIndex));
+	return true;
+}
+
+void abGPU_RTStore_Destroy(abGPU_RTStore * store) {
+	if (store->countDepth != 0u) {
+		ID3D12DescriptorHeap_Release(store->i_descriptorHeapDepth);
+	}
+	if (store->countColor != 0u) {
+		ID3D12DescriptorHeap_Release(store->i_descriptorHeapColor);
+	}
+}
+
 #endif
