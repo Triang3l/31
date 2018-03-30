ef38c5037a059469a7f3f40cdd54181417cdb9d1
Triang3l <hwguy.siplus@gmail.com>
2018-03-30 15:39:42 +0300

    abGPU_InputConfig_Register: D3D uniforms

--- Code/Tri1/abGPU/abGPUi_D3D/abGPUi_D3D_Input.c
+++ Code/Tri1/abGPU/abGPUi_D3D/abGPUi_D3D_Input.c
@@ -3,13 +3,44 @@
 
 abBool abGPU_InputConfig_Register(abGPU_InputConfig * config, abGPU_Sampler const * staticSamplers) {
 	config->inputCount = abMin(config->inputCount, abGPU_InputConfig_MaxInputs);
-	D3D12_ROOT_PARAMETER rootParameters[abGPU_InputConfig_MaxInputs];
-	D3D12_DESCRIPTOR_RANGE descriptorRanges[abGPU_InputConfig_MaxInputs];
+
+	D3D12_ROOT_PARAMETER rootParameters[abGPU_InputConfig_MaxInputs + 2], * rootParameter;
+	D3D12_DESCRIPTOR_RANGE descriptorRanges[abArrayLength(rootParameters)];
 	D3D12_STATIC_SAMPLER_DESC staticSamplerDescs[abGPU_InputConfig_MaxInputs];
 	unsigned int rootParameterCount = 0u, staticSamplerCount = 0u;
+
+	if (config->uniformStages == 0u || (config->uniform32BitCount == 0u && !config->uniformUseBuffer)) { // Normalize.
+		config->uniformStages = 0u;
+		config->uniform32BitCount = 0u;
+		config->uniformUseBuffer = abFalse;
+	}
+	if (config->uniformStages != 0u) {
+		D3D12_SHADER_VISIBILITY uniformVisibility;
+		switch (config->uniformStages) {
+		case abGPU_ShaderStageBits_Vertex: uniformVisibility = D3D12_SHADER_VISIBILITY_VERTEX; break;
+		case abGPU_ShaderStageBits_Pixel: uniformVisibility = D3D12_SHADER_VISIBILITY_PIXEL; break;
+		default: uniformVisibility = D3D12_SHADER_VISIBILITY_ALL; // Either multiple bits set or compute.
+		}
+		if (config->uniform32BitCount != 0u) {
+			rootParameter = &rootParameters[rootParameterCount++]; // Always parameter 0.
+			rootParameter->ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
+			rootParameter->Constants.ShaderRegister = 0u;
+			rootParameter->Constants.RegisterSpace = 1u;
+			rootParameter->Constants.Num32BitValues = config->uniform32BitCount;
+			rootParameter->ShaderVisibility = uniformVisibility;
+		}
+		if (config->uniformUseBuffer) {
+			rootParameter = &rootParameters[rootParameterCount++]; // Parameter 0 or 1.
+			rootParameter->ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+			rootParameter->Descriptor.ShaderRegister = 1u;
+			rootParameter->Descriptor.RegisterSpace = 1u;
+			rootParameter->ShaderVisibility = uniformVisibility;
+		}
+	}
+
 	for (unsigned int inputIndex = 0u; inputIndex < config->inputCount; ++inputIndex) {
 		abGPU_Input * input = &config->inputs[inputIndex];
-		D3D12_ROOT_PARAMETER * rootParameter = &rootParameters[rootParameterCount];
+		rootParameter = &rootParameters[rootParameterCount];
 		if (input->stages == 0u) {
 			config->i_rootParameters[inputIndex] = UINT8_MAX; // Input not used by any shader stage (a hole in the input list).
 			continue;
@@ -20,18 +51,11 @@ abBool abGPU_InputConfig_Register(abGPU_InputConfig * config, abGPU_Sampler cons
 		default: rootParameter->ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL; // Either multiple bits set or compute.
 		}
 		D3D12_DESCRIPTOR_RANGE * descriptorRange = &descriptorRanges[rootParameterCount];
-		descriptorRange->RegisterSpace = 0u;
 		descriptorRange->OffsetInDescriptorsFromTableStart = 0u;
 		switch (input->type) {
-		case abGPU_Input_Type_Constant:
-			rootParameter->ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS;
-			rootParameter->Constants.ShaderRegister = input->parameters.constant.bufferIndex;
-			rootParameter->Constants.RegisterSpace = 0u;
-			rootParameter->Constants.Num32BitValues = input->parameters.constant.count32Bits;
-			break;
 		case abGPU_Input_Type_ConstantBuffer:
 			rootParameter->ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
-			rootParameter->Descriptor.ShaderRegister = input->parameters.constantBuffer.bufferIndex;
+			rootParameter->Descriptor.ShaderRegister = input->parameters.constantBuffer.constantBufferIndex;
 			rootParameter->Descriptor.RegisterSpace = 0u;
 			break;
 		case abGPU_Input_Type_ConstantBufferHandle:
@@ -40,12 +64,8 @@ abBool abGPU_InputConfig_Register(abGPU_InputConfig * config, abGPU_Sampler cons
 			rootParameter->DescriptorTable.pDescriptorRanges = descriptorRange;
 			descriptorRange->RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_CBV;
 			descriptorRange->NumDescriptors = input->parameters.constantBufferHandle.count;
-			descriptorRange->BaseShaderRegister = input->parameters.constantBufferHandle.bufferFirstIndex;
-			break;
-		case abGPU_Input_Type_Uniform:
-			rootParameter->ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
-			rootParameter->Descriptor.ShaderRegister = input->parameters.uniform.bufferIndex;
-			rootParameter->Descriptor.RegisterSpace = 0u;
+			descriptorRange->BaseShaderRegister = input->parameters.constantBufferHandle.constantBufferFirstIndex;
+			descriptorRange->RegisterSpace = 0u;
 			break;
 		case abGPU_Input_Type_StructureBufferHandle:
 			rootParameter->ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
@@ -53,7 +73,8 @@ abBool abGPU_InputConfig_Register(abGPU_InputConfig * config, abGPU_Sampler cons
 			rootParameter->DescriptorTable.pDescriptorRanges = descriptorRange;
 			descriptorRange->RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
 			descriptorRange->NumDescriptors = input->parameters.structureBufferHandle.count;
-			descriptorRange->BaseShaderRegister = input->parameters.structureBufferHandle.imageFirstIndex;
+			descriptorRange->BaseShaderRegister = input->parameters.structureBufferHandle.structureBufferFirstIndex;
+			descriptorRange->RegisterSpace = 1u;
 			break;
 		case abGPU_Input_Type_EditBufferHandle:
 			rootParameter->ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
@@ -61,7 +82,8 @@ abBool abGPU_InputConfig_Register(abGPU_InputConfig * config, abGPU_Sampler cons
 			rootParameter->DescriptorTable.pDescriptorRanges = descriptorRange;
 			descriptorRange->RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_UAV;
 			descriptorRange->NumDescriptors = input->parameters.editBufferHandle.count;
-			descriptorRange->BaseShaderRegister = input->parameters.editBufferHandle.editFirstIndex;
+			descriptorRange->BaseShaderRegister = input->parameters.editBufferHandle.editBufferFirstIndex;
+			descriptorRange->RegisterSpace = 1u;
 			break;
 		case abGPU_Input_Type_ImageHandle:
 			rootParameter->ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
@@ -69,7 +91,8 @@ abBool abGPU_InputConfig_Register(abGPU_InputConfig * config, abGPU_Sampler cons
 			rootParameter->DescriptorTable.pDescriptorRanges = descriptorRange;
 			descriptorRange->RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
 			descriptorRange->NumDescriptors = input->parameters.imageHandle.count;
-			descriptorRange->BaseShaderRegister = input->parameters.imageHandle.imageFirstIndex;
+			descriptorRange->BaseShaderRegister = input->parameters.imageHandle.textureFirstIndex;
+			descriptorRange->RegisterSpace = 0u;
 			break;
 		case abGPU_Input_Type_EditImageHandle:
 			rootParameter->ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
@@ -77,20 +100,21 @@ abBool abGPU_InputConfig_Register(abGPU_InputConfig * config, abGPU_Sampler cons
 			rootParameter->DescriptorTable.pDescriptorRanges = descriptorRange;
 			descriptorRange->RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_UAV;
 			descriptorRange->NumDescriptors = input->parameters.editImageHandle.count;
-			descriptorRange->BaseShaderRegister = input->parameters.editImageHandle.editFirstIndex;
+			descriptorRange->BaseShaderRegister = input->parameters.editImageHandle.editImageFirstIndex;
+			descriptorRange->RegisterSpace = 0u;
 			break;
 		case abGPU_Input_Type_SamplerHandle:
 			if (staticSamplers == abNull) {
-				input->parameters.samplerHandle.staticIndex = abGPU_Input_SamplerDynamicOnly;
+				input->parameters.samplerHandle.staticSamplerIndex = abGPU_Input_SamplerDynamicOnly;
 			}
-			if (input->parameters.samplerHandle.staticIndex != abGPU_Input_SamplerDynamicOnly) {
+			if (input->parameters.samplerHandle.staticSamplerIndex != abGPU_Input_SamplerDynamicOnly) {
 				if (staticSamplerCount + input->parameters.samplerHandle.count > abArrayLength(staticSamplerDescs)) {
 					return abFalse;
 				}
 				for (unsigned int staticSamplerIndex = 0u; staticSamplerIndex < input->parameters.samplerHandle.count; ++staticSamplerIndex) {
 					D3D12_STATIC_SAMPLER_DESC * staticSamplerDesc = &staticSamplerDescs[staticSamplerCount++];
 					abGPUi_D3D_Sampler_WriteStaticSamplerDesc(
-							staticSamplers[input->parameters.samplerHandle.staticIndex + staticSamplerIndex], staticSamplerDesc);
+							staticSamplers[input->parameters.samplerHandle.staticSamplerIndex + staticSamplerIndex], staticSamplerDesc);
 					staticSamplerDesc->ShaderRegister = input->parameters.samplerHandle.samplerFirstIndex + staticSamplerIndex;
 					staticSamplerDesc->RegisterSpace = 0u;
 					staticSamplerDesc->ShaderVisibility = rootParameter->ShaderVisibility;
@@ -111,6 +135,7 @@ abBool abGPU_InputConfig_Register(abGPU_InputConfig * config, abGPU_Sampler cons
 		config->i_rootParameters[inputIndex] = rootParameterCount++;
 		nextInput:
 	}
+
 	D3D12_ROOT_SIGNATURE_DESC rootSignatureDesc = {
 		.NumParameters = rootParameterCount,
 		.pParameters = rootParameters,
