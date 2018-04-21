d74179b991545ee7e61ec41eb6937baf2c026718
Triang3l <hwguy.siplus@gmail.com>
2018-04-21 17:56:30 +0300

    Store previous free half index for freeing

--- Code/Tri1/abGFX/abGFX_Handles.c
+++ Code/Tri1/abGFX/abGFX_Handles.c
@@ -29,14 +29,19 @@ enum {
 	abGFXi_Handles_Block_Type_Free = 0u,
 	// The ^1 block is occupied.
 	// In <<abGFXi_Handles_Block_NextFreeHalfShift, the index of the free half within its level is located.
-	// Previous index not stored because only the first block is taken from the free list.
 	abGFXi_Handles_Block_Type_FreeHalf,
 	abGFXi_Handles_Block_Type_Split,
 	abGFXi_Handles_Block_Type_Allocated,
 	abGFXi_Handles_Block_TypeMask = 3u,
 
-	abGFXi_Handles_Block_NextFreeHalfShift = 2u,
-	abGFXi_Handles_Block_FreeHalfNone = 1u << abGFXi_Handles_LevelCount // This is the last free half at its level.
+	// For no previous/next, write the index of the same block (saves a couple of bits).
+	abGFXi_Handles_Block_FreeHalfPrevShift = 2u,
+	abGFXi_Handles_Block_FreeHalfNextShift = abGFXi_Handles_Block_FreeHalfPrevShift + abGFXi_Handles_LevelCount,
+	abGFXi_Handles_Block_FreeHalfLinkMask = (1u << abGFXi_Handles_LevelCount) - 1u,
+
+	abGFXi_Handles_Block_TotalBitCount = abGFXi_Handles_Block_FreeHalfNextShift + abGFXi_Handles_LevelCount,
+
+	abGFXi_Handles_FreeHalfFirstNone = UINT_MAX
 };
 
 // Starting from the top level (with 2 blocks), then the level with 4 blocks.
@@ -49,9 +54,7 @@ void abGFXi_Handles_Init() {
 		abFeedback_Crash("abGFX_Handles_Init", "Failed to create the store for %u texture/buffer handles.", abGFXi_Handles_HandleCount);
 	}
 	memset(abGFXi_Handles_Blocks, 0u, sizeof(abGFXi_Handles_Blocks));
-	for (unsigned int level = 0u; level < abGFXi_Handles_LevelCount; ++level) {
-		abGFXi_Handles_FirstFreeHalves[level] = abGFXi_Handles_Block_FreeHalfNone;
-	}
+	memset(abGFXi_Handles_FirstFreeHalves, 255u, sizeof(abGFXi_Handles_FirstFreeHalves));
 }
 
 abForceInline unsigned int abGFX_Handles_LevelBlockOffset(unsigned int level) {
@@ -59,6 +62,9 @@ abForceInline unsigned int abGFX_Handles_LevelBlockOffset(unsigned int level) {
 }
 
 unsigned int abGFX_Handles_Alloc(unsigned int count) {
+	abFeedback_StaticAssert(abGFXi_Handles_Block_TotalBitCount <= sizeof(abGFXi_Handles_Block) * 8u,
+			"Not enough bits in abGFXi_Handles_Block to store the type and two links for free halves.");
+
 	if (count == 0u) {
 		return abGFXi_Handles_HandleCount; // A more or less valid range (definitely not a valid index though).
 	}
@@ -111,7 +117,7 @@ unsigned int abGFX_Handles_Alloc(unsigned int count) {
 	// Find the closest free half.
 	int freeHalfLevel;
 	for (freeHalfLevel = (int) requestedLevel; freeHalfLevel >= 0; --freeHalfLevel) {
-		if (abGFXi_Handles_FirstFreeHalves[freeHalfLevel] != abGFXi_Handles_Block_FreeHalfNone) {
+		if (abGFXi_Handles_FirstFreeHalves[freeHalfLevel] != abGFXi_Handles_FreeHalfFirstNone) {
 			break;
 		}
 	}
@@ -127,12 +133,21 @@ unsigned int abGFX_Handles_Alloc(unsigned int count) {
 		blockIndex = 0u;
 		freeHalfLevel = 0;
 		abGFXi_Handles_Blocks[1u] = abGFXi_Handles_Block_Type_FreeHalf |
-				(abGFXi_Handles_Block_FreeHalfNone << abGFXi_Handles_Block_NextFreeHalfShift);
+				(1u << abGFXi_Handles_Block_FreeHalfPrevShift) | (1u << abGFXi_Handles_Block_FreeHalfNextShift);
 		abGFXi_Handles_FirstFreeHalves[0u] = 1u;
 	} else {
+		unsigned int blockOffset = abGFX_Handles_LevelBlockOffset(freeHalfLevel);
 		blockIndex = abGFXi_Handles_FirstFreeHalves[freeHalfLevel];
-		abGFXi_Handles_Block freeHalf = abGFXi_Handles_Blocks[abGFX_Handles_LevelBlockOffset(freeHalfLevel) + blockIndex];
-		abGFXi_Handles_FirstFreeHalves[freeHalfLevel] = freeHalf >> abGFXi_Handles_Block_NextFreeHalfShift;
+		abGFXi_Handles_Block freeHalf = abGFXi_Handles_Blocks[blockOffset + blockIndex];
+		unsigned int nextFreeHalfIndex = (freeHalf >> abGFXi_Handles_Block_FreeHalfNextShift) & abGFXi_Handles_Block_FreeHalfLinkMask;
+		if (nextFreeHalfIndex != blockIndex) {
+			abGFXi_Handles_Block * secondFreeHalf = &abGFXi_Handles_Blocks[blockOffset + nextFreeHalfIndex];
+			*secondFreeHalf &= ~(abGFXi_Handles_Block_FreeHalfLinkMask << abGFXi_Handles_Block_FreeHalfPrevShift);
+			*secondFreeHalf |= nextFreeHalfIndex << abGFXi_Handles_Block_FreeHalfPrevShift;
+			abGFXi_Handles_FirstFreeHalves[freeHalfLevel] = nextFreeHalfIndex;
+		} else {
+			abGFXi_Handles_FirstFreeHalves[freeHalfLevel] = abGFXi_Handles_FreeHalfFirstNone;
+		}
 	}
 
 	// Bridge with split nodes, then mark as allocated.
@@ -141,9 +156,22 @@ unsigned int abGFX_Handles_Alloc(unsigned int count) {
 		abGFXi_Handles_Blocks[blockOffset + blockIndex] = ((level == requestedLevel) ?
 				abGFXi_Handles_Block_Type_Allocated : abGFXi_Handles_Block_Type_Split);
 		if (level != freeHalfLevel) {
-			abGFXi_Handles_Blocks[blockOffset + (blockIndex ^ 1u)] = abGFXi_Handles_Block_Type_FreeHalf |
-					(abGFXi_Handles_FirstFreeHalves[level] << abGFXi_Handles_Block_NextFreeHalfShift);
-			abGFXi_Handles_FirstFreeHalves[level] = blockIndex ^ 1u;
+			unsigned int newFreeHalfIndex = blockIndex ^ 1u;
+			unsigned int * levelFirstFreeHalfIndex = &abGFXi_Handles_FirstFreeHalves[level];
+			if (*levelFirstFreeHalfIndex != abGFXi_Handles_FreeHalfFirstNone) {
+				// Link with the next one - update the previous in the next.
+				abGFXi_Handles_Block * secondFreeHalf = &abGFXi_Handles_Blocks[blockOffset + *levelFirstFreeHalfIndex];
+				*secondFreeHalf &= ~(abGFXi_Handles_Block_FreeHalfLinkMask << abGFXi_Handles_Block_FreeHalfPrevShift);
+				*secondFreeHalf |= newFreeHalfIndex << abGFXi_Handles_Block_FreeHalfPrevShift;
+				abGFXi_Handles_Blocks[blockOffset + newFreeHalfIndex] = abGFXi_Handles_Block_Type_FreeHalf |
+						(newFreeHalfIndex << abGFXi_Handles_Block_FreeHalfPrevShift) |
+						(*levelFirstFreeHalfIndex << abGFXi_Handles_Block_FreeHalfNextShift);
+			} else {
+				abGFXi_Handles_Blocks[blockOffset + newFreeHalfIndex] = abGFXi_Handles_Block_Type_FreeHalf |
+					(newFreeHalfIndex << abGFXi_Handles_Block_FreeHalfPrevShift) |
+					(newFreeHalfIndex << abGFXi_Handles_Block_FreeHalfNextShift);
+			}
+			*levelFirstFreeHalfIndex = newFreeHalfIndex;
 		}
 		if (level != requestedLevel) {
 			blockIndex <<= 1u;
