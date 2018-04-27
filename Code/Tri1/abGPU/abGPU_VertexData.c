fd6a6c8aea64e02ceffe228cb783dd2bf28ba9e1
Triang3l <hwguy.siplus@gmail.com>
2018-04-27 15:07:35 +0300

    abGPU_VertexData_Convert: Won't be adding S32 to F32

--- Code/Tri1/abGPU/abGPU_VertexData.c
+++ Code/Tri1/abGPU/abGPU_VertexData.c
@@ -11,7 +11,6 @@ void abGPU_VertexData_Convert_UNorm8ToFloat32_Array(float * target, uint8_t cons
 		abVec16u8 sixteenComponents = abVec8u16_LoadAligned(source);
 		abVec8u16 eightComponentsLow = abVec16u8_ToU16Low(sixteenComponents);
 		abVec8u16 eightComponentsHigh = abVec16u8_ToU16High(sixteenComponents);
-		// S32 to F32 is directly implemented in SSE, thus faster.
 		abVec4_StoreAligned(target, abVec4_Multiply(abVec4s32_ToF32(abVec4u32_AsS32(abVec8u16_ToU32Low(eightComponentsLow))), scale));
 		abVec4_StoreAligned(target + 4u, abVec4_Multiply(abVec4s32_ToF32(abVec4u32_AsS32(abVec8u16_ToU32High(eightComponentsLow))), scale));
 		abVec4_StoreAligned(target + 8u, abVec4_Multiply(abVec4s32_ToF32(abVec4u32_AsS32(abVec8u16_ToU32Low(eightComponentsHigh))), scale));
@@ -34,7 +33,6 @@ void abGPU_VertexData_Convert_UNorm16ToFloat32_Array(float * target, uint16_t co
 	abVec4 scale = abVec4_LoadX4(1.0f / 65535.0f);
 	while (count > 8u) {
 		abVec8u16 eightComponents = abVec8u16_LoadAligned(source);
-		// S32 to F32 is directly implemented in SSE, thus faster.
 		abVec4_StoreAligned(target, abVec4_Multiply(abVec4s32_ToF32(abVec4u32_AsS32(abVec8u16_ToU32Low(eightComponents))), scale));
 		abVec4_StoreAligned(target + 4u, abVec4_Multiply(abVec4s32_ToF32(abVec4u32_AsS32(abVec8u16_ToU32High(eightComponents))), scale));
 		source += 8u;
