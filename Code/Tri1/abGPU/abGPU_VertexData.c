ce2456ddc697ffea44e544a2711c1b4ac0b19fc0
Triang3l <hwguy.siplus@gmail.com>
2018-04-27 15:46:32 +0300

    abGPU_VertexData_Convert: Don't hardcode numeric limits

--- Code/Tri1/abGPU/abGPU_VertexData.c
+++ Code/Tri1/abGPU/abGPU_VertexData.c
@@ -3,10 +3,10 @@
 
 void abGPU_VertexData_Convert_UNorm8ToFloat32_Array(float * target, uint8_t const * source, unsigned int count) {
 	while (count != 0u && (((size_t) target & 15u) || ((size_t) source & 15u))) {
-		*(target++) = *(source++) * (1.0f / 255.0f);
+		*(target++) = *(source++) * (1.0f / UINT8_MAX);
 		--count;
 	}
-	abVec4 scale = abVec4_LoadX4(1.0f / 255.0f);
+	abVec4 const scale = abVec4_LoadX4(1.0f / UINT8_MAX);
 	while (count > 16u) {
 		abVec16u8 sixteenComponents = abVec8u16_LoadAligned(source);
 		abVec8u16 eightComponentsLow = abVec16u8_ToU16Low(sixteenComponents);
@@ -20,17 +20,17 @@ void abGPU_VertexData_Convert_UNorm8ToFloat32_Array(float * target, uint8_t cons
 		count -= 16u;
 	}
 	while (count != 0u) {
-		*(target++) = *(source++) * (1.0f / 255.0f);
+		*(target++) = *(source++) * (1.0f / UINT8_MAX);
 		--count;
 	}
 }
 
 void abGPU_VertexData_Convert_UNorm16ToFloat32_Array(float * target, uint16_t const * source, unsigned int count) {
 	while (count != 0u && (((size_t) target & 15u) || ((size_t) source & 15u))) {
-		*(target++) = *(source++) * (1.0f / 65535.0f);
+		*(target++) = *(source++) * (1.0f / UINT16_MAX);
 		--count;
 	}
-	abVec4 scale = abVec4_LoadX4(1.0f / 65535.0f);
+	abVec4 const scale = abVec4_LoadX4(1.0f / UINT16_MAX);
 	while (count > 8u) {
 		abVec8u16 eightComponents = abVec8u16_LoadAligned(source);
 		abVec4_StoreAligned(target, abVec4_Multiply(abVec4s32_ToF32(abVec4u32_AsS32(abVec8u16_ToU32Low(eightComponents))), scale));
@@ -40,7 +40,7 @@ void abGPU_VertexData_Convert_UNorm16ToFloat32_Array(float * target, uint16_t co
 		count -= 8u;
 	}
 	while (count != 0u) {
-		*(target++) = *(source++) * (1.0f / 65535.0f);
+		*(target++) = *(source++) * (1.0f / UINT16_MAX);
 		--count;
 	}
 }
